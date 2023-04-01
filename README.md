0aa56a84ea044370c4fea4674b8dfa94f02cb82c
zaheer-foo <1jt19cs111@jyothyit.ac.in>
2023-04-02 00:50:40 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Farmaholic-An Agricultural Resource App
+## Project Images : https://drive.google.com/drive/folders/1CufalRkC2-TEi-yRhBr5BlUB0yQ4Qdfi
---
+++ frontend/src/index.js
@@ -0,0 +1,55 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import axios from "axios";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./context/auth";
+import { SearchProvider } from "./context/search";
+import { CartProvider } from "./context/cart";
+import "antd/dist/reset.css";
+
+axios.defaults.baseURL = "https://farmaholic.onrender.com";
+
+axios.interceptors.request.use(
+  (request) => {
+    console.log(request);
+    // Edit request config
+    return request;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
+
+axios.interceptors.response.use(
+  (response) => {
+    console.log(response);
+    // Edit response config
+    return response;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <AuthProvider>
+    <SearchProvider>
+      <CartProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </CartProvider>
+    </SearchProvider>
+  </AuthProvider>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
